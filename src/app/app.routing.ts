75598b58e76c055a3369593da05c981c64d57636
maher <mahertun5@gmail.com>
2025-03-30 18:10:55 +0100

    login almsot done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,14 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 const routes: Routes =[
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: '',
     redirectTo: 'dashboard',
@@ -13,14 +18,14 @@ const routes: Routes =[
   }, {
     path: '',
     component: AdminLayoutComponent,
-    children: [
+    children: [   
         {
       path: '',
       loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
   }]},
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'login'
   }
 ];
 
